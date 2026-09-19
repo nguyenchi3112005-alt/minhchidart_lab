2aa24b8290467481e2c88985d14cc582c4df875c
nguyenchi3112005-alt <nguyenchi3112005@gmail.com>
2026-09-19 01:01:38 +0000

    Lab 1: Add age and multiplication table

--- bai2/bin/bai2.dart
+++ bai2/bin/bai2.dart
@@ -1,8 +1,17 @@
 void main() {
   final ten = 'Sinh vien';
+  final int tuoi = 20;
+
   print('Xin chao, $ten! Day la chuong trinh Dart dau tien.');
+  print('Nam sau ban se ${tuoi + 1} tuoi');
 
   for (var i = 1; i <= 5; i++) {
     print('Dem: $i');
+  }
+
+  int i = 1;
+  while (i <= 10) {
+    print('5 x $i = ${5 * i}');
+    i++;
   }
 }
